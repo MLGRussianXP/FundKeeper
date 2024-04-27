381afe3c8d543b28115f40c4b378f7fc96b590ff
MLGRussianXP <daniilkoles47@gmail.com>
2024-04-27 17:47:43 +0500

    Create basic Transaction model and a simple home layout

--- app/src/main/java/dev/dkqz/fundkeeper/HomeFragment.java
+++ app/src/main/java/dev/dkqz/fundkeeper/HomeFragment.java
@@ -8,6 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+import models.Transaction;
+
 
 public class HomeFragment extends Fragment {
     public HomeFragment() {}
@@ -15,6 +24,20 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Transaction transaction = new Transaction(
+                Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid(),
+                "test",
+                Transaction.TransactionType.INCOME,
+                "test",
+                1000,
+                0,
+                new ArrayList<>(Arrays.asList(Transaction.Category.CLOTHES, Transaction.Category.ENTERTAINMENT))
+        );
+
+        DatabaseReference push = Transaction.transactions.push();
+        transaction.setKey(push.getKey());
+        push.setValue(transaction);
+
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
---
+++ app/src/main/java/models/Transaction.java
@@ -0,0 +1,134 @@
+package models;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+public class Transaction implements Serializable {
+    public static enum TransactionType {
+        INCOME,
+        EXPENSE
+    }
+
+    public static enum Category {
+        FOOD,
+        HOUSE,
+        TRANSPORT,
+        HEALTH,
+        ENTERTAINMENT,
+        CLOTHES,
+        EDUCATION,
+        CONNECTIVITY,
+        TRAVEL,
+        AUTOMOBILE,
+        INVESTMENT,
+        OTHER
+    }
+
+    private String key = "";
+    private String ownerUid = "";
+
+    private String title = "";
+    private TransactionType type = TransactionType.INCOME;
+    private String description = "";
+    private long amount = 0;
+    private long date = 0;
+    private ArrayList<Category> categories = new ArrayList<>();
+
+    public static FirebaseDatabase db = FirebaseDatabase.getInstance();
+    public static DatabaseReference transactions = db.getReference().child("Transactions");
+
+    public Transaction(String ownerUid, String title, TransactionType type, String description, long amount, long date, ArrayList<Category> categories) {
+        this.ownerUid = ownerUid;
+        this.title = title;
+        this.type = type;
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+        this.categories = categories;
+    }
+
+    public Transaction() {}
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getOwnerUid() {
+        return ownerUid;
+    }
+
+    public void setOwnerUid(String ownerKey) {
+        this.ownerUid = ownerKey;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public void setType(TransactionType type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public ArrayList<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(ArrayList<Category> categories) {
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "title='" + title + '\'' +
+                ", type=" + type +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", date=" + date +
+                ", categories=" + categories +
+                '}';
+    }
+}
