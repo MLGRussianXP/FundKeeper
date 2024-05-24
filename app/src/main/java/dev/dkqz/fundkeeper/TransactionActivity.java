b91363d24a26b84dd30c99ed88db1925dd6467da
MLGRussianXP <daniilkoles47@gmail.com>
2024-05-24 16:14:02 +0500

    Make the transactions details view activity

--- app/src/main/java/dev/dkqz/fundkeeper/TransactionActivity.java
+++ app/src/main/java/dev/dkqz/fundkeeper/TransactionActivity.java
@@ -2,6 +2,8 @@ package dev.dkqz.fundkeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +18,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 import models.Account;
 import models.Transaction;
@@ -43,9 +48,32 @@ public class TransactionActivity extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot ds : snapshot.getChildren()) {
                     Transaction tr = ds.getValue(Transaction.class);
-                    if (tr != null)
+                    if (tr != null) {
                         transaction = tr;
+                        break;
+                    }
                 }
+
+                ((TextView) findViewById(R.id.tvName)).setText(transaction.getTitle());
+                ((TextView) findViewById(R.id.tvDescription)).setText(transaction.getDescription());
+
+                TextView amount = findViewById(R.id.tvAmount);
+                amount.setText(transaction.getAmount() + "₸");
+                if (transaction.getType() == Transaction.TransactionType.EXPENSE)
+                    amount.setTextColor(getColor(R.color.red));
+                else
+                    amount.setTextColor(getColor(R.color.green));
+
+                ArrayList<String> categories = new ArrayList<>();
+                for (Transaction.Category category : transaction.getCategories()) {
+                    if (category != null)
+                        categories.add(category.name());
+                }
+                ((TextView) findViewById(R.id.tvCategories)).setText("Categories: " + String.join(", ", categories));
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(transaction.getDate());
+                ((TextView) findViewById(R.id.tvDatetime)).setText(DateFormat.format("HH:mm, dd.MM.yyyy", calendar));
             }
 
             @Override
